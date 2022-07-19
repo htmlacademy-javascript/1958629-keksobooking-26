2e2dbce6e631959561c9f91f28ad453418dac51b
VlasGor <gorbachevvlas@gmail.com>
2022-07-19 14:46:10 +0300

    Готово №2

--- bs-config.cjs
+++ bs-config.cjs
@@ -19,14 +19,8 @@ module.exports = {
 
           if (results.filter(({ messages }) => messages.length).length) {
             console.log(formatter.format(results));
-<<<<<<< HEAD
           }
           this.reload();
-=======
-          } else {
-            this.reload();
-          }
->>>>>>> 1063da9f3e481fc69d5f7e493d5d4bc127874e93
         } catch (err) {
           process.exitCode = 1;
           console.error(err);
